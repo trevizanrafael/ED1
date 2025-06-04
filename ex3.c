13880972fe1ec799d43d1d33325c34daa73b4dd5
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-06-03 22:09:03 -0300

    Create ex3.c

---
+++ ex3.c
@@ -0,0 +1,148 @@
+/*
+3. Desenvolva um algoritmo para inverter a posição dos elementos de uma pilha P. Você
+pode criar pilhas auxiliares, se necessário. Use o push e pop. O resultado precisa ser
+dado na pilha P via parâmetro.
+void inverter (Pilha *P);
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct No{
+	int info;
+	struct no *prox;
+};
+ typedef struct No no;
+
+struct Pilha{
+	no *topo;
+};
+typedef struct Pilha pilha;
+
+pilha* cria(){
+	pilha *n;
+	n = (pilha*)malloc(sizeof(pilha));
+	n->topo = NULL;
+	return n;
+}
+
+void push(pilha *p, int v){
+	no *n;
+	n = (no*)malloc(sizeof(no));
+	if(n != NULL){
+		n->info = v;
+		n->prox = p->topo;
+		p->topo = n;
+	}
+	else{
+		printf("\nERRO DE ALOCACAO\n");
+		return NULL;
+	}
+
+		
+}
+
+
+void pop(pilha *p){
+	no *aux;
+	if(p->topo == NULL){
+		printf("\n\nPILHA VAZIA\n");
+		return;
+	} else {
+		aux = p->topo;
+		p->topo = p->topo->prox;
+		free(aux);
+		return;
+	}	
+	printf("\nERRO\n");
+}
+
+void imprimir(pilha *p){
+	
+	if(p == NULL){
+		printf("\n\nPILHA VAZIA\n");
+		return;
+	}
+	
+	no *aux = p->topo;
+	while (aux != NULL){
+		printf("%d -> ",aux->info);
+		aux = aux->prox;
+	}
+}
+
+pilha* libera(pilha *p){
+	no *aux1 = p->topo;
+	no *aux2;
+	while(aux1 != NULL){
+		aux2 = aux1;
+		aux1= aux1->prox;
+		free(aux2);
+	}
+	free(p);
+	return NULL;
+}
+
+int testaMaisElementos(pilha *p1, pilha *p2){
+	int a=0, b=0;
+	pilha *aux1 = p1;
+	pilha *aux2 = p2;
+	
+	while(aux1->topo->prox != NULL){
+		a++;
+		aux1->topo = aux1->topo->prox;
+	}
+	while(aux2->topo->prox != NULL){
+		b++;
+		aux2->topo = aux2->topo->prox;
+	}
+	if(a==b)
+		return 2;
+	if(a>b)
+		return 1;
+	if(a<b)
+		return 0;
+}
+
+void inverter(pilha *p1){
+    pilha *aux1 = cria();
+    pilha *aux2 = cria();
+    int temp;
+
+    while (p1->topo != NULL) {
+        temp = p1->topo->info;
+        pop(p1);
+        push(aux1, temp);
+    }
+    
+    while (aux1->topo != NULL) {
+        temp = aux1->topo->info;
+        pop(aux1);
+        push(aux2, temp);
+    }
+    
+    while (aux2->topo != NULL) {
+        temp = aux2->topo->info;
+        pop(aux2);
+        push(p1, temp);
+    }
+}
+
+
+int main(){
+	pilha *p1;
+	p1 = cria();
+	push(p1, 1);
+	push(p1, 2);
+	push(p1, 3);
+	push(p1, 4);
+	push(p1, 5);
+	imprimir(p1);
+	printf("\n\n\n");
+	
+	
+	inverter(p1);
+	imprimir(p1);
+	
+	return 0;
+}
